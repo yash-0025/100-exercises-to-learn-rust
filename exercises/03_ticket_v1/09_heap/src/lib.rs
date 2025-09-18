61634e5e9a152bcb5aa69f1eea29a6710bfb8bda
yash-0025 <yashpatel200403@gmail.com>
2025-09-19 02:35:50 +0530

    ✅ TICKET V1 3.9 SOLVED

--- exercises/03_ticket_v1/09_heap/src/lib.rs
+++ exercises/03_ticket_v1/09_heap/src/lib.rs
@@ -11,9 +11,10 @@ mod tests {
     use super::Ticket;
     use std::mem::size_of;
 
+    // The string on stack will only store the data like pointer, length and capacity and as it is a 8 byte each value which means 24 bytes
     #[test]
     fn string_size() {
-        assert_eq!(size_of::<String>(), todo!());
+        assert_eq!(size_of::<String>(), 24 );
     }
 
     #[test]
@@ -23,6 +24,7 @@ mod tests {
         // but, in general, the memory layout of structs is a more complex topic.
         // If you're curious, check out the "Type layout" section of The Rust Reference
         // https://doc.rust-lang.org/reference/type-layout.html for more information.
-        assert_eq!(size_of::<Ticket>(), todo!());
+        // AS string on stack has only values and not the data so 24+24+24
+        assert_eq!(size_of::<Ticket>(), 72);
     }
 }
