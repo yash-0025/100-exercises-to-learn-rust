8d4d768a57d0c0a87e1d1ce912fbc22d4bf83cda
yash-0025 <yashpatel200403@gmail.com>
2025-09-19 01:55:41 +0530

    ✅ TICKET V1 3.7 SOLVED

--- exercises/03_ticket_v1/07_setters/src/lib.rs
+++ exercises/03_ticket_v1/07_setters/src/lib.rs
@@ -11,27 +11,32 @@ pub struct Ticket {
 
 impl Ticket {
     pub fn new(title: String, description: String, status: String) -> Ticket {
-        if title.is_empty() {
-            panic!("Title cannot be empty");
-        }
-        if title.len() > 50 {
-            panic!("Title cannot be longer than 50 bytes");
-        }
-        if description.is_empty() {
-            panic!("Description cannot be empty");
-        }
-        if description.len() > 500 {
-            panic!("Description cannot be longer than 500 bytes");
-        }
-        if status != "To-Do" && status != "In Progress" && status != "Done" {
-            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
-        }
-
+        
+        valid_title(&title);
+        valid_description(&description);
+        valid_status(&status);
+        
+        
         Ticket {
             title,
             description,
             status,
         }
+    }
+
+    pub fn set_title(&mut self, title: String) {
+        valid_title(&title);
+        self.title = title;
+    }
+
+    pub fn set_description(&mut self, description: String) {
+        valid_description(&description);
+        self.description = description;
+    }
+
+    pub fn set_status(&mut self, status: String) {
+        valid_status(&status);
+        self.status = status;
     }
 
     pub fn title(&self) -> &String {
@@ -43,9 +48,33 @@ impl Ticket {
     }
 
     pub fn status(&self) -> &String {
-        &self.status
+       &self.status
     }
 }
+
+
+    fn valid_title(title: &String) {
+        if title.is_empty() {
+            panic!("Title cannot be empty");
+        }
+        if title.len() > 50 {
+            panic!("Title cannot be longer than 50 bytes");
+        }
+    }
+
+    fn valid_description(description: &String) {
+        if description.is_empty() {
+            panic!("Description cannot be empty");
+        }
+        if description.len() > 500 {
+            panic!("Description cannot be longer than 500 bytes");
+        }
+    }
+    fn valid_status(status: &String) {
+        if status != "To-Do" && status != "In Progress" && status != "Done" {
+            panic!("Only `To-Do`, `In Progress`, and `Done` statuses are allowed");
+        }
+    }
 
 #[cfg(test)]
 mod tests {
