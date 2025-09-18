6cb6a1acfa620cc8a29e99dbf1d0cbdca4a1c161
yash-0025 <yashpatel200403@gmail.com>
2025-09-18 17:01:04 +0530

    ✅ TICKET V1 3.3 SOLVED

--- exercises/03_ticket_v1/03_modules/src/lib.rs
+++ exercises/03_ticket_v1/03_modules/src/lib.rs
@@ -1,7 +1,7 @@
 mod helpers {
     // TODO: Make this code compile, either by adding a `use` statement or by using
     //  the appropriate path to refer to the `Ticket` struct.
-
+    use super::Ticket;
     fn create_todo_ticket(title: String, description: String) -> Ticket {
         Ticket::new(title, description, "To-Do".into())
     }
@@ -11,7 +11,7 @@ struct Ticket {
     title: String,
     description: String,
     status: String,
-}
+} 
 
 impl Ticket {
     fn new(title: String, description: String, status: String) -> Ticket {
