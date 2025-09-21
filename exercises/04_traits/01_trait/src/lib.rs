a6d367f8d69b2a7a3de45d0abd9efbd313799359
yash-0025 <yashpatel200403@gmail.com>
2025-09-21 23:12:39 +0530

    ✅ TRAIT 4.1 SOLVED

--- exercises/04_traits/01_trait/src/lib.rs
+++ exercises/04_traits/01_trait/src/lib.rs
@@ -3,6 +3,28 @@
 //
 // Then implement the trait for `u32` and `i32`.
 
+trait IsEven {
+    fn is_even(self) -> bool;
+}
+
+impl IsEven for u32 {
+    fn is_even(self) -> bool {
+        if self % 2 == 0 {
+           return true
+        }
+        false
+    }
+}
+impl IsEven for i32 {
+    fn is_even(self) -> bool {
+        if self % 2 == 0 {
+           return true
+        }
+        false
+    }
+}
+
+
 #[cfg(test)]
 mod tests {
     use super::*;
