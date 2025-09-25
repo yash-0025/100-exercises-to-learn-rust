654cf8ec80fa2c72e8079a17ed867582d8a6613c
yash-0025 <yashpatel200403@gmail.com>
2025-09-25 18:54:37 +0530

    ✅ TRAIT 4.6, 4.7, 4.8, 4.9, 4.10 SOLVED

--- exercises/04_traits/09_from/src/lib.rs
+++ exercises/04_traits/09_from/src/lib.rs
@@ -3,6 +3,11 @@
 pub struct WrappingU32 {
     value: u32,
 }
+impl From<u32> for WrappingU32 {
+    fn from(value: u32) -> Self {
+        WrappingU32 { value }
+    }
+}
 
 fn example() {
     let wrapping: WrappingU32 = 42.into();
