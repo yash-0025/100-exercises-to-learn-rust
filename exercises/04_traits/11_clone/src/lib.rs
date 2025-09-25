3738557be417632518534d69c73540329ab2fb65
yash-0025 <yashpatel200403@gmail.com>
2025-09-25 22:46:06 +0530

    ✅ TRAIT 4.11 SOLVED

--- exercises/04_traits/11_clone/src/lib.rs
+++ exercises/04_traits/11_clone/src/lib.rs
@@ -2,9 +2,9 @@
 //  to get the code to compile.
 
 pub fn summary(ticket: Ticket) -> (Ticket, Summary) {
-    (ticket, ticket.summary())
+    (ticket.clone(), ticket.summary())
 }
-
+#[derive(Clone)]
 pub struct Ticket {
     pub title: String,
     pub description: String,
